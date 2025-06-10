4d1345e2a104cc298a410c7ad15aa6a8fb1d8a96
Julian James Villalva Laguna <jjvlaguna@addunet.ph>
2025-06-10 15:09:53 +0800

    initial Commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,23 @@
 import { PrismaClient } from '@prisma/client';
+import { start } from 'repl';
 const prisma = new PrismaClient();
 
 async function seedflection() {
-    const election = await prisma
+    const election = await prisma.election.create({
+        where: {
+            id: 'election-2025'
+        },
+        update: {
+
+        },
+        create: {
+            id: 'election-2025',
+            name: 'Election 2025',
+            startDate: new Date('2025-01-01'),
+            endDate: new Date('2025-12-31'),
+            isActive: true
+        }
+    })
 }
 
 async function main() {
