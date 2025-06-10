43c79456832a7fc7227fa2ef781aad7a58e8444e
James-L-ctrl <Lagunaj.james@gmail.com>
2025-06-10 06:56:26 +0000

    initial commit

---
+++ prisma/seed.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function seedflection() {
+    const election = await prisma
+}
+
+async function main() {
+    console.log('seeding database')    
+}
