9b41076bd8b6449cb30dbee93f395fd92d516d8c
James-L-ctrl <Lagunaj.james@gmail.com>
2025-06-10 07:50:30 +0000

    initial commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,43 @@
-import { PrismaClient } from '@prisma/client';
-import { start } from 'repl';
+import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
+const PRESIDENT25_ID = "election-2025-president";
 
-async function seedflection() {
-    const election = await prisma.election.upsert({
-        where: {
-            id: 'election-2025'
-        },
-        update: {
+async function seedElection() {
+const election25 = await prisma.election.upsert({
+where: { id: 'election-2025' },
+update: {},
+create: {
+id: 'election-2025',
+name: 'Election 2025',
+startDate: new Date('2025-05-26T00:00:00Z'),
+endDate: new Date('2025-06-26T23:59:59Z'),
+description: 'University Student Council Elections 2025',
+isActive: true
+}
+});
 
+await prisma.position.upsert({
+    where: { id: PRESIDENT25_ID },
+    update: {},
+    create: {
+      id: PRESIDENT25_ID,
+      title: "President",
+      Election: {
+        connect: {
+          id: election25.id,
         },
-        create: {
-            id: 'election-2025',
-            name: 'Election 2025',
-            startDate: new Date('2025-01-01'),
-            endDate: new Date('2025-12-31'),
-            isActive: true
-        }
-    })
+      },
+    },
+  });
 }
 
 async function main() {
-    console.log('seeding database')    
+console.log("SEEDING DATABASE...");
+
+await seedElection();
+
+console.log("Finished SEEDING");
 }
+
+main();
